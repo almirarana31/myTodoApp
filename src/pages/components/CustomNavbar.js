4b91f666a7cdb46180ca62f7f41e176a80b1a5b1
almirarana31 <144789177+almirarana31@users.noreply.github.com>
2025-03-15 00:09:53 +0700

    remove some stuff

--- src/pages/components/CustomNavbar.js
+++ src/pages/components/CustomNavbar.js
@@ -2,26 +2,29 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { useNavigate } from "react-router-dom";
-import logo from "../../assets/logo.png"; // Ensure the logo path is correct
-import "../../styles/HomeStyles.css";
+import logo from "../../assets/logo.png";
+import "../../styles/NavbarStyles.css";
 
 const CustomNavbar = () => {
   const navigate = useNavigate();
   return (
-      <Navbar collapseOnSelect expand="lg" className="custom-navbar">
-        <Container>
-          <Navbar.Brand href="/home">Ally's To-do App</Navbar.Brand>
-          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-          <Navbar.Collapse id="responsive-navbar-nav">
-            <Nav className="me-auto">
-              <Nav.Link onClick={() => navigate("/profile")}>Profile</Nav.Link>
-              <Nav.Link onClick={() => navigate("/settings")}>Settings</Nav.Link>
-              <Nav.Link onClick={() => navigate("/")}>Logout</Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-    );
+    <Navbar collapseOnSelect expand="lg" className="custom-navbar">
+      <Container className='nav-container'>
+        <Navbar.Brand href="/home">
+          <img src={logo} alt="Logo" className="navbar-logo"/>
+          Ally's To-do App
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="ms-auto">
+          <Nav.Link onClick={() => navigate("/home")}>Home</Nav.Link>
+            <Nav.Link onClick={() => navigate("/profile")}>Profile</Nav.Link>
+            <Nav.Link onClick={() => navigate("/")}>Logout</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 };
 
 export default CustomNavbar;
